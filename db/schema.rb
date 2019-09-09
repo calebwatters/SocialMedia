b87b8c5c7573aa8137a565ed0a2f25fc9116d0b7
calebwatters <caleb@h2os.org>
2019-09-09 12:44:20 -0700

    added foreign key to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_190556) do
+ActiveRecord::Schema.define(version: 2019_09_09_192900) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_09_09_190556) do
     t.integer "likes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
